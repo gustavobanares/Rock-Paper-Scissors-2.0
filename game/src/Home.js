1fa0a9c5ad4583bc2c949a2a3f25dbb1a33b6c5e
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-09-06 23:26:03 -0300

    Create react and tailwind settings

---
+++ game/src/Home.js
@@ -0,0 +1,11 @@
+function Home(){
+    return(
+        <body>
+        <div class="text-3xl font-bold text-gray-900 text-left mt-3">
+        Jokenpô
+        </div>
+        </body>
+    )
+}
+
+export default Home
